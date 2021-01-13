0ef37aa176aa9bd36ec085a218fe6833d0bd4c97
RurouniKenshin24 <ogzhn_9225@hotmail.com>
2021-01-13 13:11:56 -0600

    Add constructor on BasePage.java

--- src/test/java/com/sfo/spartan/pages/BasePage.java
+++ src/test/java/com/sfo/spartan/pages/BasePage.java
@@ -1,5 +1,10 @@
 package com.sfo.spartan.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
-    //sorry
+
+    public BasePage(){
+    }
 }
